1451a08c7a0f408436058047d9c0c26c6fc820b0
mign0n <mign0n.git@gmail.com>
2023-04-24 11:12:37 +0500

    refactor(token): Increase the token lifetime.

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -102,7 +102,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-   'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+   'ACCESS_TOKEN_LIFETIME': timedelta(days=15),
    'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
