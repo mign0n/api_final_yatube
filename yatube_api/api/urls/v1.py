4d1693f45919ade736701b038095d5afa069a644
mign0n <mign0n.git@gmail.com>
2023-04-29 11:49:52 +0500

    refactor(api.urls): Use an additional router instead of regular expression.

--- yatube_api/api/urls/v1.py
+++ yatube_api/api/urls/v1.py
@@ -1,5 +1,5 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from api.views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
@@ -7,14 +7,13 @@ router = DefaultRouter()
 router.register('follow', FollowViewSet, basename='follow')
 router.register('groups', GroupViewSet)
 router.register('posts', PostViewSet)
-router.register(
-    'posts/(?P<post_id>.+)/comments',
-    CommentViewSet,
-    basename='post-comments',
-)
+
+comments_router = SimpleRouter()
+comments_router.register('comments', CommentViewSet, basename='post-comments')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('', include('djoser.urls')),
     path('', include('djoser.urls.jwt')),
+    path('posts/<int:post_id>/', include(comments_router.urls)),
 ]
